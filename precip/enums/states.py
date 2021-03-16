9bb8873a33bd6ff55840f6d40defa4475f9c999f
Dan Kolbman <dankolbman@gmail.com>
2021-03-15 21:01:50 -0400

    :wrench: Add enums for US states

---
+++ precip/enums/states.py
@@ -0,0 +1,60 @@
+"""
+Defines the mapping of states to their encoded forms
+"""
+from enum import Enum
+
+
+class StateEnum(Enum):
+    AL = 1
+    AZ = 2
+    AR = 3
+    CA = 4
+    CO = 5
+    CT = 6
+    DE = 7
+    FL = 8
+    GE = 9
+    ID = 10
+    IL = 11
+    IN = 12
+    IO = 13
+    KA = 14
+    KY = 15
+    LO = 16
+    ME = 17
+    MD = 18
+    MA = 19
+    MI = 20
+    MN = 21
+    MS = 22
+    MO = 23
+    MT = 24
+    NE = 25
+    NV = 26
+    NH = 27
+    NJ = 28
+    NM = 29
+    NY = 30
+    NC = 31
+    ND = 32
+    OH = 33
+    OK = 34
+    OR = 35
+    PA = 36
+    RI = 37
+    SC = 38
+    SD = 39
+    TE = 40
+    TA = 41
+    UT = 42
+    VT = 43
+    VA = 44
+    WA = 45
+    WV = 46
+    WI = 47
+    WY = 48
+    AK = 50
+    HI = 51
+    PR = 66
+    VI = 67
+    PI = 91
